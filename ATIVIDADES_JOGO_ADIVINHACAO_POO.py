fea0b374e71b23236aa5f59776bcd940e831ff57
Poliane F De Godoy <122944387+pollygodoy@users.noreply.github.com>
2023-07-20 10:04:31 -0300

    Add files via upload

---
+++ ATIVIDADES_JOGO_ADIVINHACAO_POO.py
@@ -0,0 +1,72 @@
+import random
+
+class JogoAdivinhacao:
+    def __init__(self, limite_inferior: int, limite_superior: int, max_tentativas: int):
+        self.limite_inferior = limite_inferior
+        self.limite_superior = limite_superior
+        self.max_tentativas = max_tentativas
+        self.numero_secreto = random.randint(limite_inferior, limite_superior)
+        self.tentativas = 0
+
+    def jogar(self):
+        print("Bem-vindo ao jogo de adivinhação de números!")
+        print("Vamos personalizar o jogo!")
+
+        print(f"\nOK! Estou pensando em um número entre {self.limite_inferior} e {self.limite_superior}.")
+        print(f"Você tem no máximo {self.max_tentativas} tentativas.")
+
+        while self.tentativas < self.max_tentativas:
+            palpite = int(input("\nDigite o seu palpite: "))
+            self.tentativas += 1
+
+            if palpite < self.numero_secreto:
+                print("Tente um número maior!")
+            elif palpite > self.numero_secreto:
+                print("Tente um número menor!")
+            else:
+                print(f"Parabéns! Você acertou o número em {self.tentativas} tentativas!")
+                break
+
+        if self.tentativas == self.max_tentativas:
+            print(f"\nSuas tentativas acabaram! O número correto era {self.numero_secreto}.")
+
+        self.jogar_novamente()
+
+    def jogar_novamente(self):
+        jogar_novamente = input("\nDeseja jogar novamente? (s/n): ")
+        if jogar_novamente.lower() == "s":
+            self.__init__(self.limite_inferior, self.limite_superior, self.max_tentativas)
+            self.jogar()
+        else:
+            print("\nObrigado por jogar! Até a próxima.")
+
+limite_inferior = int(input("Digite o limite inferior do intervalo de números: "))
+limite_superior = int(input("Digite o limite superior do intervalo de números: "))
+max_tentativas = int(input("Digite o número máximo de tentativas: "))
+
+jogo = JogoAdivinhacao(limite_inferior, limite_superior, max_tentativas)
+
+jogo.jogar()
+
+
+
+
+#Digite o limite inferior do intervalo de números: 5
+#Digite o limite superior do intervalo de números: 10
+#Digite o número máximo de tentativas: 1
+#Bem-vindo ao jogo de adivinhação de números!
+#Vamos personalizar o jogo!
+
+#OK! Estou pensando em um número entre 5 e 10.
+#Você tem no máximo 1 tentativas.
+
+#Digite o seu palpite: 15
+#Tente um número menor!
+
+#Suas tentativas acabaram! O número correto era 10.
+
+#Deseja jogar novamente? (s/n): n
+
+#Obrigado por jogar! Até a próxima.
+
+
